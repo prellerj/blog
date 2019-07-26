f3ced6265656fb2b145d85c0521e39e0aff77209
Jan Preller <jan@preller.me>
2019-07-27 00:26:22 +0200

    abouterino

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Homerino</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">Abouterino</router-link>
     </div>
     <router-view/>
   </div>
